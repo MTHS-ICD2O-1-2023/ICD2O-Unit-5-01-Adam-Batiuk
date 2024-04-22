89a62f1f30d13f304f68ff9b94a8822024c8a1e0
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-04-22 18:42:19 +0000

    js

--- js/script.js
+++ js/script.js
@@ -4,22 +4,10 @@
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
 
-"use strict"
+const sidea = parseInt(document.getElementById("side-a").val
 
 function myButtonClicked() {
   // Define the lengths of the three sides of a triangle
-  const sidea = parseInt(document.getElementById("side-a").value)
-  const sideb = parseInt(document.getElementById("side-b").value)
-  const sidec = parseInt(document.getElementById("side-c").value)
-
-  //Calculate the semi-perimeter of the triangle
-  const s = (sidea + sideb + sidec) / 2
-
-  // Use Heron's formula to calculate the area of the triangle
-  const area = Math.sqrt(s * ((s - sidea) * (s - sideb) * (s - sidec)))
-  const roundArea = Math.round(area * 100) / 100
-  const perimeter = s * 2
-
   if ($area !== $area) {
     document.getElementById("answer").innerHTML = "Area is not a number"
   }
